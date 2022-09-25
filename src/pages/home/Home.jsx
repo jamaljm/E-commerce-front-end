76d0773f339b3a2622637a7eac8af59ce12a1379
jam-dev <jamalvga2002@gmail.com>
2022-09-25 14:01:13 +0530

    fetured page added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import "./home.css"
 
 export default function home() {
   return (
     <div className='home'>
-      home
+      <FeaturedInfo />
     </div>
   )
 }
