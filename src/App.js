688cc5be3502a9f4f615015d0e77d0971d5fc54d
jam-dev <jamalvga2002@gmail.com>
2022-09-25 13:48:59 +0530

    side bar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import Navbar from "./components/topbar"
+import Navbar from "./components/topbar/topbar"
+import Sidebar from "./components/sidebar/Sidebar";
+import "./app.css"
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <div className="container">
+          <Sidebar />
+      </div>
     </div>
   );
 }
