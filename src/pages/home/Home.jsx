d4144fb8cee7e79d5d1a1a109ee64bf49330985b
jam-dev <jamalvga2002@gmail.com>
2022-09-30 19:18:50 +0530

    roter added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import "./home.css";
 import Chart from "../../components/chart/Chart";
-import { userData } from "./dummyData";
+import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 
---
+++ src/pages/products/product.jsx
@@ -0,0 +1,9 @@
+import "./product.css";
+
+
+export default function ProductList() {
+  
+  return (
+      <h2>product</h2>
+  );
+}
